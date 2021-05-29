eb6a9fd9ad4a29d9b1e836e155fae06c9dfa0757
Denis Gallegos <denis.gallegos03@gmail.com>
2021-05-28 19:48:11 -0600

    casi terminado

--- main.cpp
+++ main.cpp
@@ -48,21 +48,141 @@ int main(int argc, char** argv) {
 			case 2:
 				{
 				cout << "-------AGREGAR ARCHIVO------" <<endl; 
-				
+				int opcion2 = 0; 
+				while (opcion2 != 4){
+					cout << "--------MENU--------" <<endl;
+					cout << " 1) Agregar CPP. "<<endl;
+					cout << " 2) Agregar JPG. " <<endl; 
+					cout << " 3) Agregar PPT. " <<endl; 
+					cout << " 4) Salir." <<endl; 
+					cout << "---------------------" <<endl; 
+					cout << " Ingrese la opcion: " <<endl; 
+					cin>> opcion2; 
+					cin.ignore();
+					if (opcion2 == 4){
+						cout << "Ha elegido salir del menu. " <<endl; 
+					}else if (opcion2 > 0 && opcion2 <4){
+						break; 
+					}
+				}
+				string autor = "";
+				string titulo = "";
+				cout << "Ingrese el autor del archivo: " <<endl; 
+				getline(cin, autor); 
+				cout << "Ingrese el titulo del archivo: " <<endl; 
+				getline(cin, titulo);
+				switch (opcion2){
+						case 1:
+							{
+							int lineas; 
+							int errores; 
+							bool validar = false; 
+							while (validar == false){
+								cout << "Ingrese la cantidad de lineas: " <<endl; 
+								cin >> lineas; 
+								cin.ignore();
+								cout << "Ingrese la cantidad de errores: " <<endl; 
+								cin >> errores; 
+								cin.ignore();
+								if (lineas > 0 && errores >= 0){
+									validar = true;
+								}else {
+									cout << "datos invalidos." <<endl;
+								}
+							}
+							fs->GETroot()->agregararchivo(new CPP(titulo, autor, fs->GETroot(), lineas, errores));
+							break;	
+							}
+						case 2:
+							{
+							double largo; 
+							double ancho; 
+							int flash; 
+							string resolucion = "";
+							bool validar = false; 
+							while (validar == false){
+								cout << "Ingrese si tiene flash (1/0): " <<endl; 
+								cin >> flash;
+								cin.ignore();
+								cout << "Ingrese la resolucion: " <<endl; 
+								getline(cin, resolucion);
+								cout << "Ingrese el largo: " <<endl; 
+								cin >> largo; 
+								cin.ignore();
+								cout << "Ingrese el ancho: " <<endl; 
+								cin >> ancho; 
+								cin.ignore();
+								if (largo > 0 && ancho > 0 && (flash == 0 || flash == 1)){
+									validar = true;
+								}else {
+									cout << "datos invalidos." <<endl;
+								}
+							}
+							if (flash = 0){
+								validar = false; 
+							}else {
+								validar = true; 
+							}
+							fs->GETroot()->agregararchivo(new JPG(titulo, autor, fs->GETroot(), validar, resolucion, largo, ancho));
+							break; 	
+							}	
+						case 3:
+							{
+							double plantilla; 
+							double slides; 
+							int flash; 
+							string titulo = "";
+							bool validar = false; 
+							while (validar == false){
+								cout << "Ingrese si tiene animaciones (1/0): " <<endl; 
+								cin >> flash;
+								cin.ignore();
+								cout << "Ingrese el titulo: " <<endl; 
+								getline(cin, titulo);
+								cout << "Ingrese la plantilla: " <<endl; 
+								cin >> plantilla; 
+								cin.ignore();
+								cout << "Ingrese las slides: " <<endl; 
+								cin >> slides; 
+								cin.ignore();
+								if (slides > 0 && plantilla > 0 && (flash == 0 || flash == 1)){
+									validar = true;
+								}else {
+									cout << "datos invalidos." <<endl;
+								}
+							}
+							if (flash = 0){
+								validar = false; 
+							}else {
+								validar = true; 
+							}
+							fs->GETroot()->agregararchivo(new PPT(titulo, autor, fs->GETroot(), titulo, slides, plantilla, validar));
+							break;
+							}
+				}//Fin del switch
 				cout << "-------FINAL AGREGAR ARCHIVO------" <<endl; 
 				break; 
 				}
 			case 3: 
 				{
+					string autor = "";
+					string titulo = "";
 				cout << "-------AGREGAR DIRECTORIO------" <<endl; 
-				
+				cout << "Ingrese el autor del directorio: " <<endl; 
+				getline(cin, autor); 
+				cout << "Ingrese el nombre del titulo: " <<endl; 
+				getline(cin, titulo);
+				fs->GETroot()->agregardirectorio(new Directorio(titulo, autor, fs->GETroot()));
 				cout << "-------FINAL AGREGAR DIRECTORIO------" <<endl; 	
 				break; 
 				}
 			case 4:
 				{
+				string buscar = "";
 				cout << "-------IMPRIMIR POR NOMBRE------" <<endl; 	
-			
+				cout << "Ingrese el nombre de busqueda: " <<endl; 
+				getline(cin, buscar);
+				fs->GETroot()->buscarporstring(buscar);
 				cout << "-------FINAL IMPRIMIR POR NOMBRE------" <<endl; 	
 				break; 
 				}
